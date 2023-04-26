9aee186907eaca7edbb5d250d28c10393eb1749c
Jens Meisner <jnsmsnr@gmail.com>
2023-04-26 08:37:55 +0200

    diff between dev and build

---
+++ vite.dev.config.ts
@@ -0,0 +1,24 @@
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+import { defineConfig } from "vite";
+
+const certificateDirectory = "C:/development/git/jm-castle-ac-dc-client/cert";
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: "desktop-61mus1j",
+    port: 3000,
+    https: {
+      cert: fs.readFileSync(
+        path.join(certificateDirectory, "DESKTOP-61MUS1J.crt"),
+        "utf-8"
+      ),
+      key: fs.readFileSync(
+        path.join(certificateDirectory, "DESKTOP-61MUS1J.key"),
+        "utf-8"
+      ),
+    },
+  },
+  plugins: [react()],
+});
